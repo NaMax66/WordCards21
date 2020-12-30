0cb45962a2afd7b9964a5cbed887ffaae70f6549
namax66 <max.naid@mail.ru>
2020-12-30 16:28:58 +0500

    add click listener to main btn

--- app/src/main/java/com/example/wordcard21/MainActivity.kt
+++ app/src/main/java/com/example/wordcard21/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wordcard21
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAdd: Button = findViewById(R.id.btnAdd)
+        btnAdd.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
     }
 }
